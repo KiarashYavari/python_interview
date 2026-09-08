eafff93cdad6aaf6204de9c3c09c35e39ff82534
KiarashYavari <kiarash996@gmail.com>
2026-09-08 11:45:44 -0700

    find the first non repeating item in a list

--- DSA/hash_tables/frequency_counting.py
+++ DSA/hash_tables/frequency_counting.py
@@ -11,4 +11,25 @@ def frequency_counting(nums: list)-> dict:
             result_dict[num] = 1
     return result_dict
 
-print(frequency_counting(nums=nums))
+# print(frequency_counting(nums=nums))
+
+nums = [4, 5, 1, 2, 1, 4, 5]
+# Return the first number that appears exactly once.
+def none_repeating_num(nums: list)->int:
+    frequency_counting = {}
+    result = None
+    for num in nums:
+        if num in frequency_counting:
+            frequency_counting[num] += 1
+        else:
+            frequency_counting[num] = 1
+    
+    for num in nums:
+        if frequency_counting[num] == 1:
+            result = num
+            return result
+    
+    # result = next((key for key, value in frequency_counting.items() if value==1 ), None)
+    return result
+
+print(none_repeating_num(nums))
