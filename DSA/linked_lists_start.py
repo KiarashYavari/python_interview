8e566e0a69cab7768dd1d23936d4d94645cef9a8
KiarashYavari <64461860+KiarashYavari@users.noreply.github.com>
2026-09-18 14:42:29 -0700

    Implement ListNode class and conversion function
    Added ListNode class and linked list to list conversion function.

--- DSA/linked_lists_start.py
+++ DSA/linked_lists_start.py
@@ -1,1 +1,20 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+node1 = ListNode(10)
+node2 = ListNode(20)
+node3 = ListNode(30)
 
+node1.next = node2
+node2.next = node3
+
+def linked_list_to_list(head):
+    current = head
+    result = []
+    while current:
+      result.append(current.val)
+      current = current.next
+    return result
+
+print(linked_list_to_list(node1))
