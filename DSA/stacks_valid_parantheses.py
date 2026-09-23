22f3d737369dba1d3daa068209254962d708d0cd
KiarashYavari <64461860+KiarashYavari@users.noreply.github.com>
2026-09-23 16:19:22 -0700

    Add valid_parentheses function for string validation
    Implement a function to validate match paired parentheses in a string.

--- DSA/stacks_valid_parantheses.py
+++ DSA/stacks_valid_parantheses.py
@@ -1,1 +1,38 @@
+def valid_parentheses(s: str) -> bool:
+    pairs = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    }
 
+    stack = []
+
+    for char in s:
+        if char in "([{":
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+
+            if pairs[char] != stack[-1]:
+                return False
+
+            stack.pop()
+
+    return len(stack) == 0
+  
+print(valid_parentheses("())"))
+# valid_parentheses("()")
+# # True
+
+# valid_parentheses("()[]{}")
+# # True
+
+# valid_parentheses("(]")
+# # False
+
+# valid_parentheses("([)]")
+# # False
+
+# valid_parentheses("{[]}")
+# # True
